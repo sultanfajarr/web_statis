843989239cd0bfa40d22c9304c956b5b28c762ef
sultan <sultanfajar371@gmail.com>
2023-11-19 21:48:29 +0700

    baru

---
+++ script.js
@@ -0,0 +1,6 @@
+const menu = document.querySelector('.navbar #menu');
+const nav = document.querySelector('.navbar .navbar-nav');
+
+menu.addEventListener('click', function() {
+    nav.classList.toggle('active')
+});
